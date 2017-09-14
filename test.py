60f8b503a0137fc789ac76ecb12542038f6b6b15
Jurre <jurre.vandermaazen@student.hu.nl>
2017-09-14 09:59:46 +0200

    Verbeterd 14-09-2017

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-days = ("monday")
-print (type(days))
+#je krijgt eerst te vraag hoe oud je bent en daarna de vraag of je een nederlands paspoort heb. Bij vaiable "leeftijd"-
+#heb ik "eval" gezet vanwege de sting omgezet moet worden naar een integer. De if statement luid, if (als) de ingevulde-
+#"leeftijd"gelijk of hoger is dan 18 en als "paspoort" gelijk is aan "ja" dan wordt de zin "Gefeliciteerd je mag stemmen"-
+#geprint. Alle fouten antwoorden (else) worden omgezet naar "Helaas je mag niet stemmen"
+leeftijd = eval(input("Geef je leeftijd: "))
+paspoort = (input("Ben je in bezit van een Nederlands paspoort? "))
+
+if leeftijd >= 18 and paspoort == "ja":
+    print("Gefeliciteerd je mag stemmen")
+else:
+    print("Helaas je mag niet stemmen")
